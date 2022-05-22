88fed8cbe72e349dec58c536b4abad7e823c4535
Michał Bielecki <mb9246@gmail.com>
2022-05-22 14:01:22 +0200

    add var and index.html

---
+++ functions.js
@@ -0,0 +1,14 @@
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+var computerMove, player_move, playerMove, set_playerMove_to;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';

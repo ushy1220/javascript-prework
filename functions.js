916532727e079458ecb1037976e227c4f23ab5a2
Michał Bielecki <mb9246@gmail.com>
2022-05-26 13:39:10 +0200

    add buttons

--- functions.js
+++ functions.js
@@ -1,14 +1,47 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+var button_paper, button_scisors, buttonTest;
+
+/**
+ * Describe this function...
+ */
+
+buttonTest = document.getElementById('button-test');
+buttonTest.addEventListener('click', function(){ buttonClicked('Guzik TEST'); });
+button_rock = document.getElementById('button-rock');
+button_rock.addEventListener('click', function(){ buttonClicked('kamień'); });
+button_paper = document.getElementById('button-paper');
+button_paper.addEventListener('click', function(){ buttonClicked('papier'); });
+button_scisors = document.getElementById('button-scisors');
+button_scisors.addEventListener('click', function(){ buttonClicked('nożyce'); });
+
+function buttonClicked(button_paper) {
+  clearMessages();
+  console.log(' został kliknięty' + button_paper);
+
+function buttonClicked(button_rock) {
+  clearMessages();
+  console.log(' został kliknięty' + button_rock);
+
+function buttonClicked(button_scisors) {
+  clearMessages();
+  console.log(' został kliknięty' + button_scisors);
+
+buttonTest.addEventListener('click', function(){
+  buttonClicked('Test button')
+
+var playerInput, playerMove;
+
+playerMove = argButtonName;
+
+console.log('Wpisana odpowiedź to: ' + playerInput);
+
+if (playerInput == '1') {
+  playerMove = 'kamień';
+
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+
+} else {
+  playerMove = 'nożyce';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-var computerMove, player_move, playerMove, set_playerMove_to;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
+printMessage('Twój ruch: ' + playerMove);

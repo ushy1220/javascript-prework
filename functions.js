f97ed7b0f5b5238ae8588dfb2188ead58ac0d29b
Michał Bielecki <mb9246@gmail.com>
2022-05-27 13:00:56 +0200

    from begining, add playerMove

--- functions.js
+++ functions.js
@@ -1,47 +1,15 @@
-var button_paper, button_scisors, buttonTest;
-
-/**
- * Describe this function...
- */
-
-buttonTest = document.getElementById('button-test');
-buttonTest.addEventListener('click', function(){ buttonClicked('Guzik TEST'); });
-button_rock = document.getElementById('button-rock');
-button_rock.addEventListener('click', function(){ buttonClicked('kamień'); });
-button_paper = document.getElementById('button-paper');
-button_paper.addEventListener('click', function(){ buttonClicked('papier'); });
-button_scisors = document.getElementById('button-scisors');
-button_scisors.addEventListener('click', function(){ buttonClicked('nożyce'); });
-
-function buttonClicked(button_paper) {
-  clearMessages();
-  console.log(' został kliknięty' + button_paper);
-
-function buttonClicked(button_rock) {
-  clearMessages();
-  console.log(' został kliknięty' + button_rock);
-
-function buttonClicked(button_scisors) {
-  clearMessages();
-  console.log(' został kliknięty' + button_scisors);
-
-buttonTest.addEventListener('click', function(){
-  buttonClicked('Test button')
-
-var playerInput, playerMove;
-
-playerMove = argButtonName;
-
-console.log('Wpisana odpowiedź to: ' + playerInput);
-
-if (playerInput == '1') {
-  playerMove = 'kamień';
-
-} else if (playerInput == '2') {
-  playerMove = 'papier';
-
-} else {
-  playerMove = 'nożyce';
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
 }
 
-printMessage('Twój ruch: ' + playerMove);
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+
+var computerMove, playerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');

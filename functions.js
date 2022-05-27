29ff55ba26a4ac88fb1f4d28a4d4ff608e435eda
Michał Bielecki <mb9246@gmail.com>
2022-05-27 13:30:41 +0200

    add paper and scisors

--- functions.js
+++ functions.js
@@ -1,15 +1,11 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
 }
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-var computerMove, playerMove;
-computerMove = 'kamień';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+printMessage('Mój ruch: ' + computerMove);
